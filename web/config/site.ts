810fa88cf8890f033c95b66cd3fa10dd2da82634
Saheb Giri <iamsahebgiri@gmail.com>
2023-05-10 22:13:42 +0530

    refactor: move backend to api folder and add web dir

---
+++ web/config/site.ts
@@ -0,0 +1,18 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "BetaBuzz",
+  description:
+    "Creating a buzz around the latest beta products",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/iamsahebgiri",
+    github: "https://github.com/iamsahebgiri",
+    docs: "https://betabuzz.vercel.app",
+  },
+}
