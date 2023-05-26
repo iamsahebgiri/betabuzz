4a9be9b3f7b41d1baedced0cdc9911cec9e92aa5
Saheb Giri <iamsahebgiri@gmail.com>
2023-05-26 17:34:37 +0530

    feat(api): add default avatar uri

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -3,7 +3,11 @@ const catchAsync = require('../utils/catchAsync');
 const { authService, userService, tokenService, emailService } = require('../services');
 
 const register = catchAsync(async (req, res) => {
-  const user = await userService.createUser(req.body);
+  const body = {
+    ...req.body,
+    avatar: `https://ui-avatars.com/api/?name=${req.body.name}&background=random`,
+  };
+  const user = await userService.createUser(body);
   const tokens = await tokenService.generateAuthTokens(user);
   res.status(httpStatus.CREATED).send({ user, tokens });
 });
